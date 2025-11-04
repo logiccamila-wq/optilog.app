af5ca6ca76204850c638b9ca24b2f53a7d623587
logiccamila-wq <logiccamila@gmail.com>
2025-11-04 01:04:33 -0300

    feat: add complete centralized UI component exports

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,4 +1,30 @@
-export { default as Button } from './button';
-export { default as Card } from './card';
-export { default as Input } from './input';
-// Nota: Badge será adicionado quando estiver disponível
+// UI Components Library - Centralized Exports
+// OptiLog.app Design System
+// Created: 2024-11-04
+
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Card, CardHeader, CardBody, CardFooter } from './Card';
+export type { CardProps } from './Card';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { LoadingSkeleton } from './LoadingSkeleton';
+export type { LoadingSkeletonProps } from './LoadingSkeleton';
+
+export { Alert } from './Alert';
+export type { AlertProps } from './Alert';
+
+export { Badge } from './Badge';
+export type { BadgeProps } from './Badge';
+
+export { Modal } from './Modal';
+export type { ModalProps } from './Modal';
+
+export { Tooltip } from './Tooltip';
+export type { TooltipProps } from './Tooltip';
+
+export { EmptyState } from './EmptyState';
+export type { EmptyStateProps } from './EmptyState';
